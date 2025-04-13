ae195b7ec16911c033a4515c08fbdec3a195e823
Blooym <git@blooym.dev>
2025-04-13 03:50:19 +0100

    refactor: move comp-time options to main

--- xfer-client/src/commands/mod.rs
+++ xfer-client/src/commands/mod.rs
@@ -2,8 +2,4 @@ mod download;
 mod upload;
 
 pub use download::DownloadCommand;
-use std::time::Duration;
 pub use upload::UploadCommand;
-
-const PROGRESS_BAR_TICKRATE: Duration = Duration::from_millis(200);
-const DEFAULT_SERVER_URL: &str = "https://xfer.blooym.dev/";
