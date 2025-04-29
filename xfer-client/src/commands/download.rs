9f773f457313859e5c3e3f53e2912b9b7843d8d5
Lyna <git@blooym.dev>
2025-04-29 03:04:48 +0100

    feat: larger file support (#3)
    * refactor: handle all files on fs instead of memory
    
    * refactor: cleanup size fmt
    
    * refactor: remove blocking feature
    
    * refactor: add prefix to tmp dirs
    
    * refactor: genericise cryptography and api handling
    
    * chore: disable unused features
    
    * refactor: re-add public to cache headers
    
    * refactor: save space by overwriting reader
    
    * fix: pwsh doesn't handle single quotes well
    
    Signed-off-by: Lyna <19539165+Blooym@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Lyna <19539165+Blooym@users.noreply.github.com>

--- xfer-client/src/commands/download.rs
+++ xfer-client/src/commands/download.rs
@@ -4,10 +4,14 @@ use crate::{
 };
 use anyhow::{Context, bail};
 use clap::{Parser, ValueHint};
-use indicatif::{DecimalBytes, ProgressBar};
+use indicatif::{DecimalBytes, ProgressBar, ProgressStyle};
 use inquire::Confirm;
-use std::{fs, io::Cursor, path::PathBuf};
+use std::{
+    fs::{self},
+    path::PathBuf,
+};
 use tar::Archive;
+use tempfile::tempfile;
 use url::Url;
 
 /// Download and decrypt a transfer from a relay server.
@@ -46,13 +50,12 @@ pub struct DownloadCommand {
 }
 
 impl ExecutableCommand for DownloadCommand {
-    fn run(self) -> anyhow::Result<()> {
+    async fn run(self) -> anyhow::Result<()> {
         // Validate output directory.
         if !self.directory.exists() {
             bail!("the specified output directory does not exist");
-        }
-        if self.directory.is_file() {
-            bail!("output directory must be a directory and not a file");
+        } else if self.directory.is_file() {
+            bail!("output path must be a directory and not a file");
         }
 
         // Split the key into the appropriate parts
@@ -64,25 +67,25 @@ impl ExecutableCommand for DownloadCommand {
         // Obtain the transfer size from the server before downloading.
         // The server must send the `Content-Length` header on HEAD request
         // to display the transfer size pre-download.
-        let api_client = XferApiClient::new(self.server);
-        let human_transfer_size = {
-            let res = api_client.transfer_metadata(transfer_id).context(
+        let api_client = XferApiClient::new(&self.server);
+        let transfer_size = {
+            let res = api_client.transfer_metadata(transfer_id).await.context(
                 "failed to get transfer - transfer may have expired, transfer key may be incorrect, or server may have returned an error"
             )?;
-            DecimalBytes(
-                res.headers()
-                    .get("Content-Length")
-                    .map(|f| f.to_str().unwrap())
-                    .unwrap_or("0")
-                    .parse::<u64>()?,
-            )
+            let content_length = res
+                .headers()
+                .get("Content-Length")
+                .map(|f| f.to_str().unwrap())
+                .unwrap_or("0")
+                .parse::<u64>()?;
+            DecimalBytes(content_length)
         };
 
         // Ensure the user wants to continue.
         if !self.no_confirm
             && !Confirm::new(&format!(
                 "Are you sure you want to download this transfer ({})?",
-                human_transfer_size,
+                transfer_size,
             ))
             .with_default(false)
             .prompt()?
@@ -90,24 +93,40 @@ impl ExecutableCommand for DownloadCommand {
             return Ok(());
         }
 
-        let prog_bar =
-            ProgressBar::new_spinner().with_message("Downloading encrypted transfer archive");
+        let prog_bar = ProgressBar::new(transfer_size.0)
+            .with_message("Downloading encrypted transfer archive");
+        prog_bar.set_style(
+            ProgressStyle::default_bar()
+                .template("{msg} [{bar:40}] {bytes}/{total_bytes} @ {bytes_per_sec}")
+                .unwrap()
+                .progress_chars("##-"),
+        );
         prog_bar.enable_steady_tick(PROGRESS_BAR_TICKRATE);
 
         // Download & decrypt the archive and unpack it on disk.
-        let mut decrypted_archive = {
-            let res = api_client.download_transfer(transfer_id)?.bytes()?;
+        let mut archive = {
+            let mut archive_file = tempfile()?;
+            api_client
+                .download_transfer(
+                    transfer_id,
+                    &mut tokio::fs::File::from_std(archive_file.try_clone()?),
+                    |prog| prog_bar.set_position(prog),
+                )
+                .await?;
+            prog_bar.finish_and_clear();
+            let prog_bar = ProgressBar::new_spinner();
             prog_bar.set_message("Decrypting transfer archive");
-            let archive = Cryptography::decrypt(&res, decryption_key).context(
+            Cryptography::decrypt(decryption_key, &mut archive_file).context(
                 "failed to decrypt transfer archive - ensure you entered the transfer key correctly",
             )?;
-            Archive::new(Cursor::new(archive))
+            Archive::new(archive_file)
         };
+        let prog_bar = ProgressBar::new_spinner();
         prog_bar.set_message("Unpacking transfer archive");
         fs::create_dir_all(&self.directory)?;
-        decrypted_archive
-            .unpack(self.directory.canonicalize()?)
-            .context("failed to unpack decrypted transfer archive contents - archive file may be malformed")?;
+        archive.unpack(self.directory.canonicalize()?).context(
+            "failed to unpack decrypted transfer archive contents - archive file may be malformed",
+        )?;
         prog_bar.finish_and_clear();
 
         println!(
